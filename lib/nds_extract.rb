ba26e2f4872349d5b27d0f34cc554228dd80b968
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-24 02:00:10 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -55,7 +55,6 @@ def movies_with_director_key(name, movies_collection)
     count+= 1
   end
   array
-  
 end
 
 
@@ -71,6 +70,8 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
+  
+  
 end
 
 def movies_with_directors_set(source)
